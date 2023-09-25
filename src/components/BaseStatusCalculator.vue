907ab4ccdf681527f3a75d29665bbbceb3b0b0f7
Jasper <53468216+jamesoo760@users.noreply.github.com>
2023-09-25 19:45:38 +0800

    Add files via upload

---
+++ src/components/BaseStatusCalculator.vue
@@ -0,0 +1,60 @@
+<script setup>
+
+import { jobStatusCalculator } from '@/stores/jobStatus.js'
+const playerStatus = jobStatusCalculator()
+
+</script>
+<template>
+
+    <div class="d-flex flex-column gap-2">
+        <div class="input-group">
+            <span class="input-group-text" id="basic-addon1">Str</span>
+            <input class="form-control" type="number" min="1" max="99" v-model="playerStatus.baseStatus.str">
+            <span class="input-group-text">+</span>
+            <input class="form-control" type="number" v-model="playerStatus.jobStatus.str" disabled>
+            <span class="input-group-text">+</span>
+            <input class="form-control" type="number" v-model="playerStatus.etcStatus.str">
+        </div>
+        <div class="input-group">
+            <span class="input-group-text" id="basic-addon1">Agi</span>
+            <input class="form-control" type="number" min="1" max="99" v-model="playerStatus.baseStatus.agi">
+            <span class="input-group-text">+</span>
+            <input class="form-control" type="number" v-model="playerStatus.jobStatus.agi" disabled>
+            <span class="input-group-text">+</span>
+            <input class="form-control" type="number" v-model="playerStatus.etcStatus.agi">
+        </div>
+        <div class="input-group">
+            <span class="input-group-text" id="basic-addon1">Vit</span>
+            <input class="form-control" type="number" min="1" max="99" v-model="playerStatus.baseStatus.vit">
+            <span class="input-group-text">+</span>
+            <input class="form-control" type="number" v-model="playerStatus.jobStatus.vit" disabled>
+            <span class="input-group-text">+</span>
+            <input class="form-control" type="number" v-model="playerStatus.etcStatus.vit">
+        </div>
+        <div class="input-group">
+            <span class="input-group-text" id="basic-addon1">Int</span>
+            <input class="form-control" type="number" min="1" max="99" v-model="playerStatus.baseStatus.int">
+            <span class="input-group-text">+</span>
+            <input class="form-control" type="number" v-model="playerStatus.jobStatus.int" disabled>
+            <span class="input-group-text">+</span>
+            <input class="form-control" type="number" v-model="playerStatus.etcStatus.int">
+        </div>
+        <div class="input-group">
+            <span class="input-group-text" id="basic-addon1">Dex</span>
+            <input class="form-control" type="number" min="1" max="99" v-model="playerStatus.baseStatus.dex">
+            <span class="input-group-text">+</span>
+            <input class="form-control" type="number" v-model="playerStatus.jobStatus.dex" disabled>
+            <span class="input-group-text">+</span>
+            <input class="form-control" type="number" v-model="playerStatus.etcStatus.dex">
+        </div>
+        <div class="input-group">
+            <span class="input-group-text" id="basic-addon1">Luk</span>
+            <input class="form-control" type="number" min="1" max="99" v-model="playerStatus.baseStatus.luk">
+            <span class="input-group-text">+</span>
+            <input class="form-control" type="number" v-model="playerStatus.jobStatus.luk" disabled>
+            <span class="input-group-text">+</span>
+            <input class="form-control" type="number" v-model="playerStatus.etcStatus.luk">
+        </div>
+    </div>
+
+</template>
---
+++ src/components/StatusView.vue
@@ -0,0 +1,70 @@
+<script setup>
+
+import { jobStatusCalculator } from '@/stores/jobStatus.js'
+const playerStatus = jobStatusCalculator()
+
+// setTimeout(function(){
+    console.log(playerStatus.point)
+// }, 5000)
+playerStatus.loadStatusPointData()
+
+</script>
+
+<template>
+
+    <div class="d-flex flex-column gap-2">
+        <div class="t d-flex gap-3">
+            <div class="d-flex justify-content-between w-50">
+                <div>Atk</div>
+                <div>{{ playerStatus.atk_1 }}+{{ playerStatus.atk_2 }}</div>
+            </div>
+            <div class="d-flex justify-content-between w-50">
+                <div>Def</div>
+                <div>{{ playerStatus.def_1 }}+{{ playerStatus.def_2 }}</div>
+            </div>
+        </div>
+        <div class="t d-flex gap-3">
+            <div class="d-flex justify-content-between w-50">
+                <div>Matk</div>
+                <div>{{ playerStatus.matk_1 }}+{{ playerStatus.matk_2 }}</div>
+            </div>
+            <div class="d-flex justify-content-between w-50">
+                <div>Mdef</div>
+                <div>{{ playerStatus.mdef_1 }}+{{ playerStatus.mdef_2 }}</div>
+            </div>
+        </div>
+        <div class="t d-flex gap-3">
+            <div class="d-flex justify-content-between w-50">
+                <div>Hit</div>
+                <div>{{ playerStatus.hit_1 }}+{{ playerStatus.hit_2 }}</div>
+            </div>
+            <div class="d-flex justify-content-between w-50">
+                <div>Flee</div>
+                <div>{{ playerStatus.flee_1 }}+{{ playerStatus.flee_2 }}</div>
+            </div>
+        </div>
+        <div class="t d-flex gap-3">
+            <div class="d-flex justify-content-between w-50">
+                <div>Critical</div>
+                <div>{{ playerStatus.critical }}</div>
+            </div>
+            <div class="d-flex justify-content-between w-50">
+                <div>Aspd</div>
+                <div>{{ playerStatus.aspd }}</div>
+            </div>
+        </div>
+        <div class="t d-flex justify-content-between">
+            <div>Status Point</div>
+            <div>{{ playerStatus.statusPoint }}</div>
+        </div>
+    </div>
+
+</template>
+
+<style lang="sass" scoped>
+
+.t
+    padding: 0.375rem 0.75rem
+    border-bottom: 1px solid #e6e6e6
+
+</style>
