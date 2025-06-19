5d001f205fa13e1af4a8c1a2c5259b48667f9316
james-cco <james@cco.com.tw>
2025-06-19 17:26:41 +0800

    Update StatusView.vue

--- src/components/StatusView.vue
+++ src/components/StatusView.vue
@@ -13,7 +13,7 @@ playerStatus.loadStatusPointData()
 <template>
 
     <div class="d-flex flex-column gap-2">
-        <div class="t d-flex gap-3">
+        <div class="title d-flex gap-3">
             <div class="d-flex justify-content-between w-50">
                 <div>Atk</div>
                 <div>{{ playerStatus.atk_1 }}+{{ playerStatus.atk_2 }}</div>
@@ -23,7 +23,7 @@ playerStatus.loadStatusPointData()
                 <div>{{ playerStatus.def_1 }}+{{ playerStatus.def_2 }}</div>
             </div>
         </div>
-        <div class="t d-flex gap-3">
+        <div class="title d-flex gap-3">
             <div class="d-flex justify-content-between w-50">
                 <div>Matk</div>
                 <div>{{ playerStatus.matk_1 }}+{{ playerStatus.matk_2 }}</div>
@@ -33,7 +33,7 @@ playerStatus.loadStatusPointData()
                 <div>{{ playerStatus.mdef_1 }}+{{ playerStatus.mdef_2 }}</div>
             </div>
         </div>
-        <div class="t d-flex gap-3">
+        <div class="title d-flex gap-3">
             <div class="d-flex justify-content-between w-50">
                 <div>Hit</div>
                 <div>{{ playerStatus.hit_1 }}+{{ playerStatus.hit_2 }}</div>
@@ -43,7 +43,7 @@ playerStatus.loadStatusPointData()
                 <div>{{ playerStatus.flee_1 }}+{{ playerStatus.flee_2 }}</div>
             </div>
         </div>
-        <div class="t d-flex gap-3">
+        <div class="title d-flex gap-3">
             <div class="d-flex justify-content-between w-50">
                 <div>Critical</div>
                 <div>{{ playerStatus.critical }}</div>
@@ -53,7 +53,7 @@ playerStatus.loadStatusPointData()
                 <div>{{ playerStatus.aspd }}</div>
             </div>
         </div>
-        <div class="t d-flex justify-content-between">
+        <div class="title d-flex justify-content-between">
             <div>Status Point</div>
             <div>{{ playerStatus.statusPoint }}</div>
         </div>
@@ -63,7 +63,7 @@ playerStatus.loadStatusPointData()
 
 <style lang="sass" scoped>
 
-.t
+.title
     padding: 0.375rem 0.75rem
     border-bottom: 1px solid #e6e6e6
 
